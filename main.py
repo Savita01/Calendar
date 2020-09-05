b38028f145a2ac276de89ebb3ed4a1b8e13e7e3d
Savita_N <62276914+Savita01@users.noreply.github.com>
2020-09-05 16:06:59 +0530

    using PyQt5

---
+++ main.py
@@ -0,0 +1,27 @@
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+import sys
+class window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("@pythonhub")
+        self.setGeometry(100,100,600,400)
+        self.UiComponents()
+        self.show()
+    def UiComponents(self):
+        calender=QCalendarWidget(self)
+        calender.setGeometry(10,10,400,250)
+        push=QPushButton("Next Year", self)
+        push.setGeometry(120,280,100,40)
+        push.clicked.connect(lambda : do_action())
+        def do_action():
+            calender.showNextYear()
+App=QApplication(sys.argv)
+window=window()
+sys.exit(App.exec())
+
+
+
